cb15e4408e3bb10502754cee1bdb51ebd0daddee
Nora Peters <norapeters5@gmail.com>
2018-04-03 15:44:57 -0700

    removes everything from routes besides resources :drivers

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,4 @@
 Rails.application.routes.draw do
-  # get 'trips/index'
-  #
-  # get 'trips/show'
-  #
-  # get 'trips/new'
-  #
-  # get 'trips/create'
-  #
-  # get 'trips/edit'
-  #
-  # get 'trips/update'
-  #
-  # get 'trips/destroy'
-  #
-  # get 'passengers/index'
-  #
-  # get 'passengers/show'
-  #
-  # get 'passengers/new'
-  #
-  # get 'passengers/create'
-  #
-  # get 'passengers/edit'
-  #
-  # get 'passengers/update'
-  #
-  # get 'passengers/destroy'
 
   resources :drivers
 
