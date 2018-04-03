40bacaa9e73d552f14261151c9969c08d299349d
Zheng Cao <chori84@gmail.com>
2018-04-03 12:10:38 -0700

    Implement new & create for passenger controller

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,15 +1,23 @@
 class PassengersController < ApplicationController
+
   def index
-    @passengers = Passenger.all
+    @passengers = Passenger.all.order("created_at DESC")
   end
 
   def show
   end
 
   def new
+    @passenger = Passenger.new
   end
 
   def create
+    @passenger = Passenger.new(passenger_params)
+    if @passenger.save
+      redirect_to passengers_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -20,4 +28,11 @@ class PassengersController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def passenger_params
+    return params.require(:passenger).permit(:name, :phone_num)
+  end
+
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,5 @@
 class Passenger < ApplicationRecord
   has_many :trips
+
+  validates_presence_of :name, :phone_num
 end
