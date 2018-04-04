c24a7c233de16f8a9b4fb02883c4c56cce07bc0c
Nora Peters <norapeters5@gmail.com>
2018-04-03 17:01:07 -0700

    modifies driver model logic and minor string errors in driver show view

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -2,11 +2,15 @@ class Driver < ApplicationRecord
   has_many :trips
 
   def total_earnings
-    return (self.trips.inject(0) { |sum, trip| sum + trip.cost } / 100)
+    return (self.trips.inject(0) { |sum, trip| sum + earnings_per_trip(trip.cost) } / 100).round(2)
   end
 
   def average_rating
     return (self.trips.inject(0) { |sum, trip| sum + trip.rating } / trips.count).round(2)
+  end
+
+  def earnings_per_trip(amount)
+    return (amount - 165) * 0.8
   end
 
 end
