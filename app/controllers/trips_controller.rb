1c743ebff47f5ae98adcf683a5fdd91bd98b4fa0
Zheng Cao <chori84@gmail.com>
2018-04-05 08:48:40 -0700

    Implement trip update controller action and redirect to show view

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -17,8 +17,26 @@ class TripsController < ApplicationController
   end
 
   def update
+    @trip = Trip.find_by(id: params[:id])
+
+    #TODO: fix bug
+    if @trip
+      if @trip.update(trip_params)
+        @trip.cost = params[:cost].to_i * 100
+        redirect_to trip_path
+      else
+        render :edit
+      end
+    end
+
   end
 
   def destroy
+  end
+
+  private
+
+  def trip_params
+    return params.require(:trip).permit(:date, :driver_id, :passenger_id, :rating, :cost)
   end
 end
